36738df44154ab0af16ac12bf20ad16aa219bb51
Patrick Ferris <patrickferris17@gmail.com>
2020-06-18 17:19:16 +0100

    Working version of the compiler for RISC-V cross compilation (#1)
    * Testing opam installation
    
    * Add modified opam files thanks to @kayceesrk

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh -e
+
+# Code written by by @kayceesrk
+
+PREFIX="$1"
+#added seq only
+for pkg in bigarray bytes compiler-libs dynlink findlib seq stdlib str threads unix; do
+  cp -r "${OPAM_SWITCH_PREFIX}/lib/${pkg}" "${OPAM_SWITCH_PREFIX}/riscv-sysroot/lib/"
+done
+sed -i -e 's/unix//g' ${OPAM_SWITCH_PREFIX}/riscv-sysroot/lib/bigarray/META
+ln -sf "${OPAM_SWITCH_PREFIX}/bin/ocamlrun" "${OPAM_SWITCH_PREFIX}/riscv-sysroot/bin/ocamlrun"
+mkdir -p "${OPAM_SWITCH_PREFIX}/lib/findlib.conf.d"
+cp riscv.conf "${OPAM_SWITCH_PREFIX}/lib/findlib.conf.d"
