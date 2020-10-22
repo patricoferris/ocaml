30facd68ccbc41925ed25752d90db02a36d402d2
patricoferris <pf341@patricoferris.com>
2020-10-22 23:11:08 +0000

    make sd sw

--- build.sh
+++ build.sh
@@ -6,11 +6,12 @@ git checkout 403042794068d29583e8b30e22bde94abaf27192
 make -j8 world.opt && make install
 export PATH="/riscv-ocaml/bin:${PATH}"
 # Checkout the latest code with the inline assembly
-git checkout 86e8e6bcca211c8618f032597ab28f0097167412 
+git checkout 2942bd7801b4460228402eeab07fbbc7f65d5015 
 make clean
 ./configure --host=riscv32-unknown-linux-gnu --prefix=/riscv-ocaml \ 
 	    --disable-ocamldoc --disable-debugger --disable-ocamltest \ 
-	    --with-target-bindir=/riscv-ocaml/bin \ 
+	    --with-target-bindir=/riscv-ocaml/bin \
+	    CC='riscv32-unknown-linux-gnu-gcc -march=rv32g -mabi=ilp32d' \ 
 	    AS='riscv32-unknown-linux-gnu-as -march=rv32g' \ 
 	    ASPP='riscv32-unknown-linux-gnu-gcc -march=rv32g -c' 
 make -j8 world
