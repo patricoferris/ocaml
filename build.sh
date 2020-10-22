91d4afc9655444db6614cf3adfd546ce034dbeac
patricoferris <pf341@patricoferris.com>
2020-10-22 17:43:55 +0000

    test copying for rv32

---
+++ build.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Checkout code before the inline assembly updates
+git checkout 403042794068d29583e8b30e22bde94abaf27192
+./configure --disable-ocamldoc --disable-debugger --disable-ocamltest --prefix=/riscv-ocaml
+make -j8 world.opt && make install
+export PATH="/riscv-ocaml/bin:${PATH}"
+# Checkout the latest code with the inline assembly
+# git checkout 86e8e6bcca211c8618f032597ab28f0097167412 
+# make clean 
+# ./configure --host=riscv32-unknown-linux-gnu --prefix=/riscv-ocaml --disable-ocamldoc --disable-debugger --disable-ocamltest --with-target-bindir=/riscv-ocaml/bin 
+#make -j8 world
+#make -j4 opt
+#cp /riscv-ocaml/bin/ocamlrun runtime 
+#make install
+#make clean
