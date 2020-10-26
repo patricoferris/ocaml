62472a427ef2c99bc60bed03fc5e3dfdc312b30d
patricoferris <pf341@patricoferris.com>
2020-10-26 19:28:21 +0000

    change build

--- build.sh
+++ build.sh
@@ -2,19 +2,22 @@
 
 # Checkout code before the inline assembly updates
 git checkout 403042794068d29583e8b30e22bde94abaf27192
-./configure --disable-ocamldoc --disable-debugger --disable-ocamltest --prefix=/riscv-ocaml
+./configure --disable-ocamldoc --disable-debugger --disable-ocamltest --prefix=/riscv-ocaml \
+	    --build=x86_64-pc-linux-gnu --host=i386-linux \
+            CC='gcc -m32' AS='as --32' ASPP='gcc -m32 -c' \
+            PARTIALLD='ld -r -melf_i386'
 make -j8 world.opt && make install
 export PATH="/riscv-ocaml/bin:${PATH}"
 # Checkout the latest code with the inline assembly
-git checkout 2942bd7801b4460228402eeab07fbbc7f65d5015 
-make clean
-./configure --host=riscv32-unknown-linux-gnu --prefix=/riscv-ocaml \ 
-	    --disable-ocamldoc --disable-debugger --disable-ocamltest \ 
-	    --with-target-bindir=/riscv-ocaml/bin \
-	    CC='riscv32-unknown-linux-gnu-gcc -march=rv32g -mabi=ilp32d' \ 
-	    AS='riscv32-unknown-linux-gnu-as -march=rv32g' \ 
-	    ASPP='riscv32-unknown-linux-gnu-gcc -march=rv32g -c' 
-make -j8 world
+#git checkout 2942bd7801b4460228402eeab07fbbc7f65d5015 
+#make clean
+#./configure --host=riscv32-unknown-linux-gnu --prefix=/riscv-ocaml \ 
+#	    --disable-ocamldoc --disable-debugger --disable-ocamltest \ 
+#	    --with-target-bindir=/riscv-ocaml/bin \
+#	    CC='riscv32-unknown-linux-gnu-gcc -march=rv32g -mabi=ilp32d' \ 
+#	    AS='riscv32-unknown-linux-gnu-as -march=rv32g' \ 
+#	    ASPP='riscv32-unknown-linux-gnu-gcc -march=rv32g -c' 
+#make -j8 world
 #make -j4 opt
 #cp /riscv-ocaml/bin/ocamlrun runtime 
 #make install
